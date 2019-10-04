0fd836cb7f36ee790a5069c767a673424c99b30c
SANPEI design <m.miyamoto.fc1@gmail.com>
2019-10-04 17:20:06 +0900

    算出プロパティ

--- docs/js/test.js
+++ docs/js/test.js
@@ -1,6 +1,11 @@
 new Vue({
-    el: '#app',
-    data: {
-        flag: true
-    }
+		el: '#app',
+		data: {
+				email: 'sanpei@exmaple.com'
+		},
+		computed: {
+			localEmail: function() {
+				return this.email.split('@')[0].toLowerCase();
+			}
+		}
 });
