4aba9234945afe2026dac1f48a639a74f6125964
SANPEI design <m.miyamoto.fc1@gmail.com>
2019-10-08 15:52:07 +0900

    this指定ミス修正

--- docs/js/test.js
+++ docs/js/test.js
@@ -10,7 +10,7 @@ new Vue({
 		Vue.set(this.author, 'company', 'WINGSプロジェクト');
 		this.$nextTick().then(function() {
 			//#app配下にcompanyプロパティの内容が含まれているか、ビューへの反映を待ってから確認
-			console.log(this.$el.textContent.includes(this.author.company));
+			console.log(that.$el.textContent.includes(that.author.company));
 		})
 	}
 });
