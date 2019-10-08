1599980ccb554bd7d5c20c72ad1e17ce734c36cf
SANPEI design <m.miyamoto.fc1@gmail.com>
2019-10-08 14:49:51 +0900

    Vue.jsはプロパティそのものの追加や削除を検知できない

--- docs/js/test.js
+++ docs/js/test.js
@@ -1,18 +1,18 @@
-new Vue({
+let app = new Vue({
 	el: '#app',
 	data: {
-		//現在日時
-		current: new Date()
+		author: {
+			name: '山田'
+		}
 	},
-	//起動時にタイマーを設定
 	created: function() {
 		let that = this;
-		//1000ミリ秒スパンでcurrenrプロパティを更新
-		this.timer = setInterval(function() {
-			that.current = new Date();
-		}, 1000);
+		//3000ミリ秒後にプロパティを追加
+		this.timer = setTimeout(function() {
+			//that.author.name = 'Y.YAMADA';
+			that.author.company = 'WINGSプロジェクト';
+		}, 3000);
 	},
-	//終了まえにタイマーを破棄
 	beforeDestroy: function() {
 		clearInterval(this.timer);
 	}
